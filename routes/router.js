34b90658d62026632044c3ba2981979091d4d41d
Filson-aj <83718775+Filson-aj@users.noreply.github.com>
2023-08-09 16:20:57 +0100

    Update router.js
    update

--- routes/router.js
+++ routes/router.js
@@ -6,5 +6,9 @@ module.exports = app =>{
     router.post('/crypto-stego/encrypt', stego.encrypt);
     router.post('/crypto-stego/decrypt', stego.decrypt);
 
+    router.get('/', (req, res) =>{
+        res.send("<h1>Welcome to stegnograph apis</h1">);
+    });
+
     app.use(router);
 };
